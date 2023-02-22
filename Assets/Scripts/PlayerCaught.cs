4453989ab6096828ca79c0dd015b6e03b84c060e
TatumCo <124401778+TatumCo@users.noreply.github.com>
2023-02-21 20:14:52 -0700

    GameOver

--- Assets/Scripts/PlayerCaught.cs
+++ Assets/Scripts/PlayerCaught.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCaught : MonoBehaviour
 {
+    public static event Action OnPlayerDeath;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.gameObject.CompareTag("Barry"))
         {
             Debug.Log("Game Over");
+            SceneManager.LoadScene(3);
         }
     }
-
 }
