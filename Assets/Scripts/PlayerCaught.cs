4cfd0f0ff1a88e420add3fef51b98460e3a6a86f
TatumCo <124401778+TatumCo@users.noreply.github.com>
2023-02-21 19:58:44 -0700

    Revert "GameOver"
    This reverts commit 0c23c75fc907c32589acf38ffbcd75b8dfa58510.

--- Assets/Scripts/PlayerCaught.cs
+++ Assets/Scripts/PlayerCaught.cs
@@ -1,18 +1,15 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCaught : MonoBehaviour
 {
-    public static event Action OnPlayerDeath;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.gameObject.CompareTag("Barry"))
         {
             Debug.Log("Game Over");
-            OnPlayerDeath?.Invoke();
         }
     }
 
